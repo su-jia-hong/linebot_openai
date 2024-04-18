9b3188b17dfaf876bfe04d85d664f36c6dccc5a8
蘇家宏 <129284722+hong91511@users.noreply.github.com>
2024-04-18 22:43:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def GPT_response(text):
     try:
         # 使用您的微調模型ID作為 model 參數的值
         response = openai.Completion.create(
-            model="ft:gpt-3.5-turbo-0125:personal:0418:9FMpdOGW",  # 這裡替換為您的微調模型ID
+            model="ft:gpt-3.5-turbo-1106:personal:1106:9FNG2Buj",  # 這裡替換為您的微調模型ID
             prompt=text,
             temperature=1.0,
             max_tokens=20
