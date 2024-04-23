93640e6a53b058824aa756f380d78d192d825bf8
蘇家宏 <129284722+hong91511@users.noreply.github.com>
2024-04-23 22:03:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -44,10 +44,6 @@ def GPT_response(text):
         print(f"Error: {e}")
         return "出現錯誤，請稍後再試。"
 
-
-    if(message == '店家資訊'):
-        FlexMessage = json.load(open('card.json','r',encoding='utf-8'))
-        line_bot_api.reply_message(reply_token, FlexSendMessage('profile',FlexMessage))
         
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -76,6 +72,10 @@ def handle_message(event):
     except:
         print(traceback.format_exc())
         line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
+
+    if(message == '店家資訊'):
+        FlexMessage = json.load(open('card.json','r',encoding='utf-8'))
+        line_bot_api.reply_message(reply_token, FlexSendMessage('profile',FlexMessage))
         
 
 @handler.add(PostbackEvent)
