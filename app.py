e24113a79575106358cd41483b6bac85afdc112c
蘇家宏 <129284722+hong91511@users.noreply.github.com>
2024-04-24 19:49:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,14 +27,23 @@ handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
+# def GPT_response(text):
+#     # 接收回應
+#     response = openai.Completion.create(model="ft:gpt-3.5-turbo-1106:personal:coffee:9HTIW0HP", prompt=text, temperature=0.5, max_tokens=50)
+#     print(response)
+#     # 重組回應
+#     answer = response['choices'][0]['text'].replace('。','')
+#     return answer
 def GPT_response(text):
-    # 接收回應
-    response = openai.Completion.create(model="ft:gpt-3.5-turbo-1106:personal:coffee:9HTIW0HP", prompt=text, temperature=0.5, max_tokens=500)
-    print(response)
-    # 重組回應
-    answer = response['choices'][0]['text'].replace('。','')
+    completion = client.chat.completions.create(
+        model="ft:gpt-3.5-turbo:my-org:custom_suffix:id",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Hello!"}
+        ]
+    )
+    answer = completion.choices[0].message.content
     return answer
-
         
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
