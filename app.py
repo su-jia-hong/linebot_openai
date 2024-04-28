abba9699a32ecd9e0a26df33c2eb2d8fc7468ad8
蘇家宏 <129284722+hong91511@users.noreply.github.com>
2024-04-28 21:40:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ except Exception as e:
 
 def GPT_response(text):
     # 接收回應
-    # response = openai.Completion.create(model="ft:gpt-3.5-turbo-1106:personal:coffee:9HTIW0HP", prompt=text, temperature=0.5, max_tokens=50)
+    # response = openai.Completion.create(model="gpt-3.5-turbo-instruct", prompt=text, temperature=0.5, max_tokens=50)
     # print(response)
     # # 重組回應
     # answer = response['choices'][0]['text'].replace('。','')
