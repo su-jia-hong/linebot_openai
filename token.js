6413fd901dea49241269f0b0e90e98260dd00373
蘇家宏 <129284722+hong91511@users.noreply.github.com>
2024-09-27 16:54:14 +0800

    Create token.js

---
+++ token.js
@@ -0,0 +1,13 @@
+{
+  "type": "service_account",
+  "project_id": "coffee-helper-435706",
+  "private_key_id": "c096f38dc14fbbafa2312b0c2f0a5a08ec2b785e",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCkE4WhFOX6FEgz\nnq0Mc1ncAtujZBGkZzByKf81wSXptVGBIfgiw1ffh+7g7AMpEzQlxyNL//6TM0ca\nvpXIEw2ZruWQs9Ta/Qv9R3jX8DJUSw2Z7MSaOZxfGkpqC949fyUqW0HpW6RxWkmh\nw7yEucoxM0UwrrV1ayAlW2xD9zXBiP3QeHes8UXyNLJTNgSDKAvdRLXLJtpVAfgs\niSSa/nm8L+LVSwX/GDon9PodCoEZQKMHkNkTUaInUr2qv3cipM3EG7mnKbVoPT+B\nxid9Yz1zRUK6d8zlNzdPbKT+dT0eEIOd/19D4cOiiRc+E5UCQG0xSKdZcofIs92l\nzlE/KIPxAgMBAAECggEABNe9VPYh8jymWOexDIdcZUhW+pPvNV6vqQmg9RGLW0I1\nHU5HCEz4YKpJca1loSZmSUspuTTdawjTa3BT9BYj4PUEOj4kj9tyKXqc5Xq7HDkk\nNfaCuFlE9UWjC/g9CgGhCNwCrlmRw/Vklk1fA2fca5FWKcwDoms+r/uVJCMSx3CF\n5rfxMBRC6lgQ6Qd114FIktQ6RdPjyD5iJ11ePKyp51TyG0fR/6JTkLvWHm57e8Su\nMppyrW5/KUlFBiNmSwdIJc6uMkRYeKBMKqK4KBHAorfSmnfdrOClbyC5kNbUhXPM\ntCGCEp53ltMYvlvhWcti8NOoyv6FOi3KzvJw99Em1QKBgQDObBxlppFtUSSit/2b\nVH8tFyaiw098k8JU6jkftXYozPO2TYGFweuX+G2dSoCyA+iVs4aMmAWkBIOosOJ/\nJoTBuCdutaaLegIPqDVT5bb0p/gp9Ae/6RRix5paVvZTtNhjgKWm9HBFQtccyAuO\nK2TQuu8dJZmideQC+QOPR78iqwKBgQDLe8LqRjzdF8dp8QTyE3fZEXLDCuhm9Q/a\nQoIZLxXkrTb0V4jsb/Oy2mvxR0aLa/be+EiYo+dgO4tplDXCaqVLph1+6v/0u0PF\nzlTbhFzT9YwBqhkJZsOLX5yOS9xBeLSNtK5wKlCwRgSH4MBgyfeG5+kvU77AFgTL\nROl5GSTT0wKBgDwdhA+eeM/LR84To0nvS7JIAbp5AtcW+QtdwxEW+bhcJ8W5wLTB\niukRMmsSXXR6auveAptDUrE3l1gzW2MIRrVb+i/luwD3oU4KAK3UQnnxyPX+2Zrs\nOEGkO7F/jfFGw5YGyzdaDLA/DQKWx7a5sHyRVD26y4lip+qttnmtvV5NAoGAXsal\nUbMnVB9oPVsKX6pbrxomZtwkxRetq45AxHQe8WUQbJx85Qt76bk8f6QgIn5F/TKN\ntbLZ6QOy+pkStRwxI1wa7uuMDjRO68of0iMDjaIX9QqCINPhjoAZDlodG82Z40rd\n2NaMwJ3MLHGtiSdpCEfqX6XU+ekHkJnnWQUyFwsCgYAU1zxhSCK7yXiIaelZ4U91\noeQlCskv2NwC4RVNWwNFVoSdC5HjLZCgz1Zvipvieeou7FmrsqV4zSVQk6Oc7QTN\nQdqVl/3dpYnp8ejjNXsjXYuyL9+jHU3UDjHHp0Xzw+IGwU20BtEb3cKvCPcacp2h\nRK94hBfdsdU2CpM6BFsBDg==\n-----END PRIVATE KEY-----\n",
+  "client_email": "coffee-helper@coffee-helper-435706.iam.gserviceaccount.com",
+  "client_id": "111712090405027505712",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/coffee-helper%40coffee-helper-435706.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
