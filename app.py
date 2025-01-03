678c74609907990c96b14ffdddfed748d80562ff
蘇家宏 <129284722+hong91511@users.noreply.github.com>
2025-01-03 22:39:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -324,9 +324,7 @@ def handle_message(event):
             ]
         )
     )
-        line_bot_api.reply_message(event.reply_token, buttons_template_message)
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+
 
     
     # # 確認訂單功能
